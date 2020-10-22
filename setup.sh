8b8ad4923495925f3e56e9c0598ce8f27d4a13c7
Dmitry <cort@mail.ru>
2020-10-22 09:55:11 +0300

    patched wordpress now working on nginx

--- setup.sh
+++ setup.sh
@@ -1,6 +1,34 @@
+#!/bin/bash
+
+echo "$1"
+
+if [ $1 = "delete" ];
+then
+    kubectl delete -f srcs/metal
+    kubectl delete -f srcs/k8s
+    minikube delete
+fi
+if [ $1 = "clean" ];
+then
+    kubectl delete -f srcs/metal
+    kubectl delete -f srcs/k8s
+fi
+if [ $1 = "restart" ];
+then
+    kubectl delete -f srcs/metal
+    kubectl delete -f srcs/k8s
+    minikube delete
+fi
+
+minikube start --driver=virtualbox
+minikube addons enable metallb
+
+#docker setup
+eval $(minikube docker-env)
+
 #metallb
-sh srcs/metal/init_metallb.sh
-kubectl apply -f ./srcs/metal
+#sh srcs/metal/init_metallb.sh
+kubectl apply -f srcs/metal
 
 #ftps
 docker build -t ftps srcs/ftps
@@ -21,4 +49,4 @@ docker build -t nginx srcs/nginx
 docker build -t wordpress srcs/wordpress
 
 #k8s
-kubectl apply -f ./srcs/k8s
+kubectl apply -f srcs/k8s
